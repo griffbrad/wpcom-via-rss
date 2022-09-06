43a22a5ed0b26114949346078c99b6ac184cedf4
Dan Q <github@danq.me>
2022-09-06 17:40:12 +0100

    Standardise indentation a bit better

--- www/includes/opml-template.php
+++ www/includes/opml-template.php
@@ -1,20 +1,20 @@
 <?php
 function escape($input)
 {
-        echo htmlspecialchars($input);
+    echo htmlspecialchars($input);
 }
 function site_url($site)
 {
-        echo preg_replace( '/\/feed$/', '', preg_replace( '/^https?:\/\//', '', $site['URL'] ) );
+    echo preg_replace( '/\/feed$/', '', preg_replace( '/^https?:\/\//', '', $site['URL'] ) );
 }
 ?>
 <?xml version="1.0" encoding="UTF-8"?><opml version="1.0">
 <head>
-        <title>WPCOM subscriptions for <?php escape($_SERVER['PHP_AUTH_USER']) ?></title>
+    <title>WPCOM subscriptions for <?php escape($_SERVER['PHP_AUTH_USER']) ?></title>
 </head>
 <body>
 <?php foreach($followed_sites as $site) { ?>
-        <outline type="rss" title="<?php site_url( $site ); ?>" xmlUrl="<?php echo BASE_URI, '/rss.php?blog=', $site['blog_ID']; ?>" />
+    <outline type="rss" title="<?php site_url( $site ); ?>" xmlUrl="<?php echo BASE_URI, '/rss.php?blog=', $site['blog_ID']; ?>" />
 <?php } ?>
 </body>
 </opml>
